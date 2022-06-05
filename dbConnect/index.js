4cacdd93d715f6d5c547893a67957fa4100cdefc
zuhriansyah surya putra <zuhri@zuhriansyahs-MacBook-Pro.local>
2022-06-05 22:08:18 +0700

    issue connect k database menggunakan ssh

---
+++ dbConnect/index.js
@@ -0,0 +1,14 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("db_middleware", "zuhri", "JuaraMobile", {
+  host: "127.0.0.1",
+  dialect: "postgres",
+  port: 5432,
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+});
+
+module.exports = { sequelize };
--- index.js
+++ index.js
@@ -2,7 +2,50 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+const { sequelize } = require("./dbConnect");
+
 require("dotenv").config();
+const { Client } = require("ssh2");
+
+const conn = new Client();
+conn
+  .on("ready", () => {
+    console.log("Client :: ready");
+    conn.exec("uptime", (err, stream) => {
+      if (err) throw err;
+      //ORM -- conection///
+      sequelize
+        .authenticate()
+        .then((db) => {
+          console.log("CONNECTION ESTABLISHED! ", db);
+        })
+        .catch((err) => {
+          console.error("UNABLE TO ESTABLISH CONNECTION: ", err.message);
+        });
+      // --///
+      stream
+        .on("close", (code, signal) => {
+          console.log(
+            "Stream :: close :: code: " + code + ", signal: " + signal
+          );
+          conn.end();
+        })
+        .on("data", (data) => {
+          console.log("STDOUT: " + data);
+        })
+        .stderr.on("data", (data) => {
+          console.log("STDERR: " + data);
+        });
+    });
+  })
+  .connect({
+    host: "103.229.161.189",
+    port: 7755,
+    username: "ibpr",
+    password: "JuaraBersama",
+    // privateKey: readFileSync("/path/to/my/key"),
+  });
 
 const port = process.env.PORT;
 
@@ -12,8 +55,17 @@ app.use(bodyParser.text());
 app.use(bodyParser.raw());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("bpr-mobile-api");
+app.get("/", async (req, res) => {
+  try {
+    console.log("Request");
+    let Request = await sequelize.query(`SELECT * FROM acct_ebpr`, {
+      type: QueryTypes.SELECT,
+    });
+
+    res.send(Request);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.listen(port, () => {
