48ac0c559335509e645cfeb58deea02a8a12a1b2
zuhriansyah surya putra <zuhri@zuhriansyahs-MacBook-Pro.local>
2022-07-14 17:19:38 +0700

    login redis

---
+++ authMiddleware/user.js
@@ -0,0 +1,80 @@
+let jwt = require("jsonwebtoken");
+const VerifyToken = async (req, res, next) => {
+  try {
+    if (!req.get("Authorization"))
+      return res.status(200).send({
+        code: "E97",
+        status: "error",
+        message: "No token provided!",
+        data: null,
+      });
+
+    const token = req.get("Authorization").split(" ")[1];
+    let FindKey = [req.body, req.query];
+
+    jwt.verify(token, process.env.SECRET_KEY, async (err, payload) => {
+      try {
+        console.log("cek", payload.id);
+        if (err) {
+          //--Error JWT--//
+          console.log("Errro JWT Verify Access Token", err.message);
+
+          res.status(200).send({
+            code: "E98",
+            status: "error",
+            message: "Unauthorized!",
+            data: null,
+          });
+          //----//
+        } else {
+          //--Verify UserID--//
+
+          if (
+            FindKey.some((val) => {
+              if (Object.values(val).some((val2) => val2 == payload.id))
+                return true;
+            })
+          ) {
+            next();
+            // let AccessTokenCheck = await Redis.get(`BL_${payload.id}`);
+            // !AccessTokenCheck || AccessTokenCheck !== AccessToken
+            //   ? next()
+            //   : res.status(200).send({
+            //       code: "E98",
+            //       status: "error",
+            //       message: "Unauthorized!",
+            //       data: null,
+            //     });
+            // //---//
+          } else {
+            console.log("Not Allow to Access..");
+            res.status(200).send({
+              code: "E98",
+              status: "error",
+              message: "Unauthorized!",
+              data: null,
+            });
+          }
+        }
+      } catch (error) {
+        console.log("error verify", error);
+        res.status(200).send({
+          code: "E99",
+          status: "error",
+          message: "failed",
+          data: null,
+        });
+      }
+    });
+  } catch (error) {
+    console.log("Error Middleware AcccesToken", error.message);
+    res.status(200).send({
+      code: "E99",
+      status: "error",
+      message: "failed",
+      data: error.message,
+    });
+  }
+};
+
+module.exports = { VerifyToken };
---
+++ utility/redis.js
@@ -0,0 +1,22 @@
+// let { createClient } = require("redis");
+
+// let UrlRedis = `redis://103.229.161.189:7755`;
+
+// let Redis = createClient({
+//   url: UrlRedis,
+// });
+
+// Redis.connect();
+// Redis.on("connect", () => {
+//   console.log("Redis Connected....");
+// });
+
+// Redis.on("error", (err) => {
+//   console.log("error", err);
+// });
+
+// Redis.on("end", () => {
+//   console.log("Client disconnected from redis");
+// });
+
+// module.exports = { Redis };
