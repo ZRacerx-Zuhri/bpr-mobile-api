903997af0ffd30319af28d77c468a57303b10414
Putra Nugroho <putraa.nugroho@gmail.com>
2022-07-15 15:11:42 +0700

    Get Product

--- controller/ppob.js
+++ controller/ppob.js
@@ -1,20 +1,47 @@
 const axios = require("../Services/API");
 
+// const productPPOB = async (req, res) => {
+//   try {
+//     let Request = await axios.get("/api/v2/bill/products");
+
+//     if (Request.data.status.code === "000") {
+//       //--berhasil dapat list product update atau insert ke db --//
+//       res.status(200).send(Request.data);
+//     } else {
+//       //--status gagal api--//
+//       res.status(200).send(Request.data);
+//     }
+//   } catch (error) {
+//     //--error server--//
+//     console.log("erro get product", error);
+//     res.send(error);
+//   }
+// };
+
 const productPPOB = async (req, res) => {
   try {
-    let Request = await axios.get("/api/v2/bill/products");
+    let {} = req.body;
+    
+    let Request = await db.sequelize.query(`SELECT A.produk_id, A.tipe_produk, A.urut_produk, A.nama_produk, B.produk_prov, B.admin_fee, B.denom, B.prioritas FROM kd_produk AS A INNER JOIN master_produk AS B ON A.produk_id = B.produk_id WHERE A.status = '1'`, {
+      replacements: [],
+      type: db.sequelize.QueryTypes.SELECT,
+    });
 
-    if (Request.data.status.code === "000") {
-      //--berhasil dapat list product update atau insert ke db --//
-      res.status(200).send(Request.data);
-    } else {
-      //--status gagal api--//
-      res.status(200).send(Request.data);
-    }
+    res.status(200).send({
+      code: "000",
+      status: "ok",
+      message: "Success",
+      data: Request,
+    });
   } catch (error) {
-    //--error server--//
-    console.log("erro get product", error);
-    res.send(error);
+    console.log("error get clinic", error);
+
+    res.status(200).send({
+      code: "E99",
+      status: "error",
+      message: error.message,
+      data: null,
+    });
   }
 };
 
