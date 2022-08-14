0fa85a49eb281db4516f767ea60713ec4666a860
Putra Nugroho <putraa.nugroho@gmail.com>
2022-08-14 09:28:50 +0700

    Get Date from DB

---
+++ utility/getDate.js
@@ -0,0 +1,16 @@
+const db = require("../dbConnect");
+
+const date = async () => {
+    const dateTimeDb = await db.sequelize.query(
+        `SELECT NOW()`,
+        {
+          type: db.sequelize.QueryTypes.SELECT,
+        }
+    );
+
+    return dateTimeDb
+}
+
+module.exports = {
+    date,
+};
